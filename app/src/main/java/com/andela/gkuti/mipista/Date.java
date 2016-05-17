5011a3e4c027730f5fd29821de01036577e8a857
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-17 08:52:14 +0100

    [Finishes #119654981] Create Date class

---
+++ app/src/main/java/com/andela/gkuti/mipista/Date.java
@@ -0,0 +1,18 @@
+package com.andela.gkuti.mipista;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class Date {
+    public static String getDate() {
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+        SimpleDateFormat sd = new SimpleDateFormat("dd/MMM/yyyy");
+        return sd.format(calendar.getTime());
+    }
+    public static String getTime() {
+        java.util.Date date = new java.util.Date();
+        SimpleDateFormat sd = new SimpleDateFormat("HH:mm:ss");
+        return sd.format(date);
+    }
+}
