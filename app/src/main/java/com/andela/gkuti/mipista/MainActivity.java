ecf1f22f9d3c72025dae20597bec24fd5fddbe6f
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-20 12:53:58 +0100

    Create dialog for picking minimum tracking time

--- app/src/main/java/com/andela/gkuti/mipista/MainActivity.java
+++ app/src/main/java/com/andela/gkuti/mipista/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        handler = new Handler();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
