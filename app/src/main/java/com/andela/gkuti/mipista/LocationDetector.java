573a99c365c64012d2f6db813478b27fc0d61b65
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-20 13:37:56 +0100

    Modify LocationDetector to return unknown when location not detected

--- app/src/main/java/com/andela/gkuti/mipista/LocationDetector.java
+++ app/src/main/java/com/andela/gkuti/mipista/LocationDetector.java
@@ -14,47 +14,39 @@ import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import java.io.IOException;
 import java.util.List;
 
-public class LocationDetector implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class LocationDetector implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener {
     private GoogleApiClient googleApiClient;
     private Context context;
-    private String country;
-    private String state;
-    private String locality;
-    private String street;
+    private String country = " ";
+    private String state = " ";
+    private String locality = " ";
+    private String street = " ";
+    private double longitude;
+    private double latitude;
 
     public LocationDetector(Context context) {
         this.context = context;
+        googleApiClient = new GoogleApiClient.Builder(context).addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-            if (location != null) {
-                double lat = location.getLatitude(), lon = location.getLongitude();
-                Log.d("ne", String.valueOf(lon) + " " + String.valueOf(lat));
-                Geocoder geocoder = new Geocoder(context);
-                List<Address> addresses = null;
-                try {
-                    addresses = geocoder.getFromLocation(lat, lon, 1);
-                    if (addresses != null && addresses.size() > 0) {
-                        Address address = addresses.get(0);
-                        country = address.getCountryName();
-                        state = address.getAdminArea();
-                        locality = address.getSubLocality();
-                        street = address.getFeatureName();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(1000);
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
         }
     }
 
@@ -69,23 +61,49 @@ public class LocationDetector implements GoogleApiClient.ConnectionCallbacks, Go
     }
 
     public void connect() {
-        googleApiClient = new GoogleApiClient.Builder(context, this, this).addApi(LocationServices.API).build();
         googleApiClient.connect();
     }
 
-    public String getCountry() {
-        return country;
+    public void disconnect() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                googleApiClient, this);
     }
 
-    public String getState() {
-        return state;
+    @Override
+    public void onLocationChanged(Location location) {
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+        detectCountry();
     }
 
-    public String getLocality() {
-        return locality;
+    private void detectCountry() {
+        Log.d("ne", String.valueOf(longitude) + " " + String.valueOf(latitude));
+        Geocoder geocoder = new Geocoder(context);
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                country = address.getCountryName();
+                state = address.getAdminArea();
+                locality = address.getSubLocality();
+                street = address.getFeatureName();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (getAddress()) {
+            disconnect();
+        }
     }
 
-    public String getStreet() {
-        return street;
+    public String getLocation() {
+        if (getAddress()) {
+            return street + " " + locality + " " + state + " " + country;
+        }
+        return "unknown";
+    }
+    public boolean getAddress(){
+        return (!country.equals(" ") || !state.equals(" ") || !locality.equals(" ") || !street.equals(" "));
     }
 }
