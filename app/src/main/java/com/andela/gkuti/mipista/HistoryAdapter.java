31ed6f83c386ce858576e2f1ae26a700fd3eca74
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-20 13:20:17 +0100

    Add HistoryAdapter class

---
+++ app/src/main/java/com/andela/gkuti/mipista/HistoryAdapter.java
@@ -0,0 +1,50 @@
+package com.andela.gkuti.mipista;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
+    private List<History> historyList;
+    private Context context;
+
+    public HistoryAdapter(List<History> historyList, Context context) {
+        this.historyList = historyList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.location_data_item, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        History history = historyList.get(position);
+        holder.startTime.setText(history.getStartTime());
+        holder.endTime.setText(history.getEndTime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return historyList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView startTime;
+        private TextView endTime;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            startTime = (TextView) itemView.findViewById(R.id.time_from);
+            endTime = (TextView) itemView.findViewById(R.id.time_to);
+        }
+    }
+}
