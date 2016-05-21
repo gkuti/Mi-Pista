ab50c71dde1e8c041f4bf953d214a668808b593c
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-21 07:47:33 +0100

    Remove navigation drawer codes from MainActivity class

--- app/src/main/java/com/andela/gkuti/mipista/MainActivity.java
+++ app/src/main/java/com/andela/gkuti/mipista/MainActivity.java
@@ -7,88 +7,49 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
     private Thread thread;
     private boolean isTracking;
     private Handler handler;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        textView = (TextView) findViewById(R.id.status);
         handler = new Handler();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.tracking_button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startTracking();
-                initthread();
-                thread.start();
+                if (!isTracking) {
+                    fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_stop_white_24dp));
+                    startTracking();
+                    initthread();
+                    thread.start();
+                    textView.setText("TRACKING");
+                } else {
+                    fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_my_location_white_24dp));
+                    isTracking = false;
+                    textView.setText("NOT TRACKING");
+                }
             }
         });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
@@ -106,10 +67,10 @@ public class MainActivity extends AppCompatActivity
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-        UserActivity userActivity = new UserActivity(this);
+        UserActivity  userActivity = new UserActivity(this);
         userActivity.connect();
         Tracker tracker = new Tracker(this);
-        tracker.start();
+        tracker.update();
         isTracking = true;
     }
 
@@ -122,15 +83,20 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
             return true;
         }
+        if (id == R.id.action_history) {
+            Intent intent = new Intent(this, LocationActivity.class);
+            startActivity(intent);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
-    public void initthread(){
+
+    public void initthread() {
         thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -149,7 +115,8 @@ public class MainActivity extends AppCompatActivity
             }
         });
     }
-    public void animate(){
+
+    public void animate() {
         YoYo.with(Techniques.Pulse).duration(2000).playOn(findViewById(R.id.outer_pulse));
         YoYo.with(Techniques.Pulse).duration(2000).playOn(findViewById(R.id.pulse_image));
         YoYo.with(Techniques.Pulse).duration(2000).playOn(findViewById(R.id.status));
