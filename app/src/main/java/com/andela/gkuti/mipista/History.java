f618392122d0c5a88d80fe3d0ffa70e0fbdff57e
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-21 18:23:45 +0100

    Add duration to model

--- app/src/main/java/com/andela/gkuti/mipista/History.java
+++ app/src/main/java/com/andela/gkuti/mipista/History.java
@@ -3,10 +3,12 @@ package com.andela.gkuti.mipista;
 public class History {
     private String startTime;
     private String endTime;
+    private String duration;
 
-    public History(String startTime, String endTime) {
+    public History(String startTime, String endTime, String duration) {
         this.startTime = startTime;
         this.endTime = endTime;
+        this.duration = duration;
     }
 
     public String getStartTime() {
@@ -15,5 +17,9 @@ public class History {
 
     public String getEndTime() {
         return endTime;
+    }
+
+    public String getDuration() {
+        return duration;
     }
 }
