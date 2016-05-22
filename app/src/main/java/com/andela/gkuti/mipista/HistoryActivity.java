d90a04e05418f35fcddf4f1dd61ee1f2c1045a16
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-22 08:14:04 +0100

    Refactor HistoryActivity class

--- app/src/main/java/com/andela/gkuti/mipista/HistoryActivity.java
+++ app/src/main/java/com/andela/gkuti/mipista/HistoryActivity.java
@@ -5,28 +5,39 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 
 import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
-    ArrayList<History> historyList = new ArrayList();
+    private ArrayList<History> historyList = new ArrayList();
+    private Datastore datastore;
+    private String location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-        String location = getIntent().getExtras().getString("location");
-        Datastore datastore = new Datastore(this);
+        location = getIntent().getExtras().getString("location");
+        datastore = new Datastore(this);
+        generateList();
+        initializeView();
+    }
+
+    private void generateList() {
         Cursor cursor = datastore.getHistoryByLocation(location);
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            String startTime = cursor.getString(cursor.getColumnIndex("StartTime"));
-            String endTime = cursor.getString(cursor.getColumnIndex("EndTime"));
-            String duration = String.valueOf(cursor.getInt(cursor.getColumnIndex("Duration")));
+            String startTime = cursor.getString(cursor.getColumnIndex(Constants.START_TIME_COLUMN.getValue()));
+            String endTime = cursor.getString(cursor.getColumnIndex(Constants.END_TIME_COLUMN.getValue()));
+            String duration = DateUtils.formatElapsedTime(cursor.getInt(cursor.getColumnIndex(Constants.DURATION_COLUMN.getValue())));
             History history = new History(startTime, endTime, duration);
             historyList.add(history);
             cursor.moveToNext();
         }
+    }
+
+    private void initializeView() {
         RecyclerView historyView = (RecyclerView) findViewById(R.id.history_list);
         HistoryAdapter historyAdapter = new HistoryAdapter(historyList, this);
         historyView.setLayoutManager(new LinearLayoutManager(this));
