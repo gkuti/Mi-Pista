5717b7171f58aa06724f81489abc52566b32f4c7
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-25 19:11:13 +0100

    Create MainActivtyTest class

---
+++ app/src/androidTest/java/com/andela/gkuti/mipista/MainActivityTest.java
@@ -0,0 +1,52 @@
+package com.andela.gkuti.mipista;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testOnCreate() throws Exception {
+        getActivity();
+        onView(withText("Mi Pista")).check(matches(isDisplayed()));
+        onView(withText("NOT TRACKING")).check(matches(isDisplayed()));
+    }
+
+    public void testLocationDetector() throws Exception {
+        getActivity();
+        Thread.sleep(5000);
+        onView(withText("Moleye St Lagos Mainland Lagos Nigeria")).check(matches(isDisplayed()));
+    }
+
+    public void testTrackerButton() throws Exception {
+        getActivity();
+        onView(withId(R.id.tracking_button)).perform(click());
+        onView(withText("TRACKING")).check(matches(isDisplayed()));
+        onView(withId(R.id.tracking_button)).perform(click());
+        onView(withText("NOT TRACKING")).check(matches(isDisplayed()));
+    }
+
+    public void testHistoryButton() throws Exception {
+        getActivity();
+        onView(withId(R.id.action_history)).perform(click());
+        onView(withText(Date.getDate())).check(matches(isDisplayed()));
+    }
+
+    public void testSettingsButton() throws Exception {
+        getActivity();
+        onView(withId(R.id.action_settings)).perform(click());
+        onView(withText("Settings")).check(matches(isDisplayed()));
+    }
+}
